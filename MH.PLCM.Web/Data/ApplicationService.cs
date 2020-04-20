98ec8587d8f42c957fb672a2c16cca17c5f947fe
Matada, Prakash <Prakash.Matada@mann-hummel.com>
2020-04-20 08:01:48 -0400

    permissions working

--- MH.PLCM.Web/Data/ApplicationService.cs
+++ MH.PLCM.Web/Data/ApplicationService.cs
@@ -17,65 +17,48 @@ namespace MH.PLCM.Data
             _db = db;
         }
 
-        public List<PermissionTree> GetPermissions(int? PermissionId)
-        {
-            var permissions = (from p in _db.Permissions
-                               where (PermissionId.HasValue ? p.PermissionParentId == PermissionId : p.PermissionParentId == 0)
-                               select new PermissionTree
-                               {
-                                   PermissionId = p.PermissionId,
-                                   PermissionName = p.PermissionName,
-                                   HasChildren = (from pc in _db.Permissions
-                                                  where (pc.PermissionParentId == p.PermissionId)
-                                                  select pc
-                                                  ).Count() > 0
-                               }).ToList();
 
-            return (permissions);
-
-        }
-
-        public  IEnumerable<TreeViewItemModel> GetPermissionsTree()
+        public IEnumerable<TreeViewItemModel> GetPermissionsTree()
         {
             var result = new List<TreeViewItemModel>();
-            var root =  (from p in _db.Permissions
+            var root = (from p in _db.Permissions
                         where (p.PermissionParentId == 0)
                         select new TreeViewItemModel
                         {
                             Id = p.PermissionId.ToString(),
                             Text = p.PermissionName,
-                            Expanded=true,
-                            Checked= true
-                            
-                            
+                            Expanded = true,
+                            Checked = true
+
+
                         }).SingleOrDefault();
 
-            if(root != null)
+            if (root != null)
             {
-               AddChildren(root);
+                AddChildren(root);
             }
             result.Add(root);
 
             return (result.AsEnumerable());
         }
 
-        public  void AddChildren(TreeViewItemModel parent)
+        public void AddChildren(TreeViewItemModel parent)
         {
-            var children =  (from pc in _db.Permissions
-                             where (pc.PermissionParentId == Convert.ToInt32(parent.Id))
-                             select new TreeViewItemModel
-                             {
-                                 Id = pc.PermissionId.ToString(),
-                                 Text = pc.PermissionName,
-                                 Expanded= true
-                                 
-                             }).ToList();
-            if(children.Count >0)
+            var children = (from pc in _db.Permissions
+                            where (pc.PermissionParentId == Convert.ToInt32(parent.Id))
+                            select new TreeViewItemModel
+                            {
+                                Id = pc.PermissionId.ToString(),
+                                Text = pc.PermissionName,
+                                Expanded = true
+
+                            }).ToList();
+            if (children.Count > 0)
             {
                 parent.HasChildren = true;
                 parent.Items = children;
 
-                foreach(var child in children)
+                foreach (var child in children)
                 {
                     AddChildren(child);
                 }
@@ -85,12 +68,7 @@ namespace MH.PLCM.Data
 
     }
 
-    public class PermissionTree
-    {
-        public int PermissionId { get; set; }
-        public string PermissionName { get; set; }
-        public bool HasChildren { get; set; }
-    }
+
 
 
 }
